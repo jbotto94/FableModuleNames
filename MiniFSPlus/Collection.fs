f44c66650ec4a6e8a6ddd47836824874a655c881
JB <jbotto94@users.noreply.github.com>
2019-09-23 19:01:23 +0100

    Uploading further Collection errors

--- MiniFSPlus/Collection.fs
+++ MiniFSPlus/Collection.fs
@@ -27,3 +27,35 @@ type Distinct =
     static member inline Distinct (x: '``Collection<'T>``  , [<Optional>]_impl: Default2) = x |> ToSeq.Invoke |> Seq.distinct |> OfSeq.Invoke         : '``Collection<'T>``
     static member inline Distinct (x: ^``Collection<'T>``  , [<Optional>]_impl: Default1) = (^``Collection<'T>`` : (static member Distinct : _->_) x) : '``Collection<'T>``
     static member inline Distinct (_: ^t when ^t : null and ^t : struct, _mthd: Default1) = id
+
+
+type Rev =
+    inherit Default1
+
+    static member        Rev (x: list<'a>   , [<Optional>]_impl: Rev  ) = List.rev x
+    static member        Rev (x: 'a []      , [<Optional>]_impl: Rev  ) = Array.rev x
+    static member inline Rev (x: '``Collection<'T>``, [<Optional>]_impl: Default2) = x |> ToSeq.Invoke |> Seq.rev |> OfSeq.Invoke         : '``Collection<'T>``
+    static member inline Rev (x: ^``Collection<'T>``, [<Optional>]_impl: Default1) = (^``Collection<'T>`` : (static member Rev : _->_) x) : '``Collection<'T>``
+    static member inline Rev (_: ^t when ^t: null and ^t: struct, _mthd: Default1) = id
+
+type Sort =
+    inherit Default1
+
+    static member        Sort (x: list<'a>, [<Optional>]_impl: Sort) = List.sort x
+    static member        Sort (x: 'a []   , [<Optional>]_impl: Sort) = Array.sort x
+    static member inline Sort (x: '``Collection<'T>``, [<Optional>]_impl: Default2) = x |> ToSeq.Invoke |> Seq.sort |> OfSeq.Invoke         : '``Collection<'T>``
+    static member inline Sort (x: ^``Collection<'T>``, [<Optional>]_impl: Default1) = (^``Collection<'T>`` : (static member Sort : _->_) x) : '``Collection<'T>``
+    static member inline Sort (_: ^t when ^t: null and ^t: struct, _mthd: Default1) = id
+
+type Split =
+    inherit Default1
+    
+    static member Split ((e: list<string>      , x: string       ), [<Optional>]_impl: Split) = x.Split (Seq.toArray e, StringSplitOptions.None) |> Array.toList
+    static member Split ((e: seq<StringBuilder>, x: StringBuilder), [<Optional>]_impl: Split) = x.ToString().Split (e |> Seq.map string |> Seq.toArray, StringSplitOptions.None) |> Array.map StringBuilder :> seq<_>
+    static member Split ((e: StringBuilder []  , x: StringBuilder), [<Optional>]_impl: Split) = x.ToString().Split (e |> Seq.map string |> Seq.toArray, StringSplitOptions.None) |> Array.map StringBuilder
+    static member Split ((e: StringBuilder list, x: StringBuilder), [<Optional>]_impl: Split) = x.ToString().Split (e |> Seq.map string |> Seq.toArray, StringSplitOptions.None) |> Array.map StringBuilder |> Array.toList
+
+ type Split with
+    static member inline Split ((e: '``'Collection<'OrderedCollection>``, x: '``'OrderedCollection``), [<Optional>]_impl: Default1) = (^``'OrderedCollection`` : (static member Split : _*_->_) e, x)                          : '``'Collection<'OrderedCollection>``
+    static member inline Split ((_: ^t when ^t: null and ^t: struct, _                              ),             _mthd: Default1) = id
+
