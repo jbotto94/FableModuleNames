40f2a538b69491ad0b988a93cfdde6ab3a32d91b
JB <jbotto94@users.noreply.github.com>
2019-09-24 14:29:18 +0100

    Update Collection.fs

--- MiniFSPlus/Collection.fs
+++ MiniFSPlus/Collection.fs
@@ -18,6 +18,28 @@ type OfSeq =
         call (Unchecked.defaultof<OfSeq>, value)
 
 
+type TestType =
+    static member TestType (x: list<'a>) = x
+    static member TestType (x: 'a []) = x
+
+
+type TestType1 =
+    static member inline TestType1 (x: list<'a>) = x
+    static member inline TestType1 (x: 'a []) = x
+
+
+type TestType2 =
+    static member inline TestType2 (x: list<'a>) = x
+    static member TestType2 (x: 'a []) = x
+
+
+
+type Distinct1 =
+//    static member        Distinct1 (x: list<'a>, [<Optional>]_impl: Distinct1) = List.distinct x
+    static member inline Distinct1 (x: ^``Collection<'T>``  , [<Optional>]_impl: Default1) = (^``Collection<'T>`` : (static member Distinct1 : _->_) x) : '``Collection<'T>``
+    static member inline Distinct1 (_: ^t when ^t : null and ^t : struct, _mthd: Default1) = id //must
+
+
 
 type Distinct =
     inherit Default1
